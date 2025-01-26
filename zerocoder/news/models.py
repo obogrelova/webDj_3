96c12972227383a0686e9b6908cd18c56303fced
obogrelova <on.obogrelova@gmail.com>
2025-01-26 14:13:18 +0500

    Update files

--- zerocoder/news/models.py
+++ zerocoder/news/models.py
@@ -6,7 +6,6 @@ class News_post(models.Model):
     short_description = models.CharField('Краткое описание новости', max_length=200)
     text = models.TextField('Новость')
     pub_date = models.DateTimeField('Дата публикации')
-    username = models.CharField('Исполнитель', max_length=60)
 
     def __str__(self):
         return self.title
