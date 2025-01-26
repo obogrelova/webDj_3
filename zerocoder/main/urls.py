e3aa7e06f4b3add211528c6b9d8050326b4f4471
obogrelova <on.obogrelova@gmail.com>
2025-01-26 17:30:00 +0500

    Delete files

--- zerocoder/main/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='home'),
-    path('newpage2', views.newpage2, name='page2'),
-    path('newpage3', views.newpage3, name='page3'),
-    path('newpage4', views.newpage4, name='page4')
-]
