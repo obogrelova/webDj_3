8f3fee04232a29b3cfbbe1993acd36f54fe41422
obogrelova <on.obogrelova@gmail.com>
2025-01-26 08:19:01 +0500

    update files

--- zerocoder/main/views.py
+++ zerocoder/main/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+def index(request):
+    return render(request, "main/index.html")
+
+def newpage2(request):
+    return render(request, "main/newpage2.html")
+
+def newpage3(request):
+    return render(request, "main/newpage3.html")
+
+def newpage4(request):
+    return render(request, "main/newpage4.html")
